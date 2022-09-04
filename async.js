2f2ddd4a673a34daa80c8f99c5fd344d4b2980c2
Chinwendu Emenike <echinwenduvictoriae@gmail.com>
2022-09-04 21:31:06 +0100

    i accessed the object directly here

--- async.js
+++ async.js
@@ -1,8 +1,21 @@
+// async function randomuser(){
+//     let randomuser = await fetch("https://randomuser.me/api")
+//     let jsonrandomuser = await randomuser.json()
+//     let filterrandomuser = jsonrandomuser.results
+//     let finalrandomuser = filterrandomuser.map(user=>`<center><img src="${user.picture.large}"><br><br><strong>First Name: ${user.name.first}<br>Last Name:${user.name.last}</strong></center>`)
+//     return document.body.innerHTML = finalrandomuser
+
+    
+// }
+
+// randomuser()
+
+
 async function randomuser(){
     let randomuser = await fetch("https://randomuser.me/api")
     let jsonrandomuser = await randomuser.json()
     let filterrandomuser = jsonrandomuser.results
-    let finalrandomuser = filterrandomuser.map(user=>`<center><img src="${user.picture.large}"><br><br><strong>First Name: ${user.name.first}<br>Last Name:${user.name.last}</strong></center>`)
+    let finalrandomuser = `<center><img src="${filterrandomuser[0].picture.large}"><br><br><strong>${filterrandomuser[0].name.first}<br>${filterrandomuser[0].name.last}</strong></center>`
     return document.body.innerHTML = finalrandomuser
 
     
